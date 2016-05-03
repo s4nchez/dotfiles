e60673328bb19babb2538b68547bf71ea24d7ca2
Ivan Sanchez <s4nchez@gmail.com>
2016-05-03 08:13:25 +0100

    git case insensitivity and more docker ports

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -8,7 +8,7 @@ function docker-machine-expose-ports {
         elif ! docker-machine ls | grep "$VM_NAME" | grep virtualbox; then
             echo -e "- ${RED}Warning: active docker-machine VM doesn't appear to use VirtualBox; ports will not be mapped.${NC}"
         else
-            for PORT in 7654 7655 7656 7657 7659 8000 8001 8002 8400 8501 9999; do
+            for PORT in 7654 7655 7656 7657 7659 7660 8000 8001 8002 8400 8501 9999; do
                 if ! VBoxManage showvminfo "$VM_NAME" 2>&1 | grep "host port = $PORT" > /dev/null; then
                     echo -e "- ${BLUE}Adding VirtualBox NAT rules for VM ${BROWN}$VM_NAME${BLUE} on port ${BROWN}$PORT${NC}"
                     if ! VBoxManage controlvm "$VM_NAME" natpf1 "marklogic-$PORT,tcp,127.0.0.1,$PORT,,$PORT" >/dev/null 2>&1; then
